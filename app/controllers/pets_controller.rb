290497dd55a2215f8eab431641538c9dacfc5d73
Kelin Field <kfield111@hotmail.com>
2020-11-02 02:30:28 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,14 +12,14 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
-    if !params["owner"]["name"].empty?
-      @pet.owner == Owner.create(name: params["owner"]["name"])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
     else
-      @pet.owner == Owner.find_by_id(params["pet"]["owner_id"])
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
-  end
+
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
